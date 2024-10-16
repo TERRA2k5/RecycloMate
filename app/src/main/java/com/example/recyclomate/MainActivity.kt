c1ca9ddfaacfd8c4c95ea1b4bdec45851aa80cb1
aryan <aryananand005@gmail.com>
2024-10-16 10:49:57 +0530

    ViewPager

--- app/src/main/java/com/example/recyclomate/MainActivity.kt
+++ app/src/main/java/com/example/recyclomate/MainActivity.kt
@@ -5,16 +5,65 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.viewpager.widget.ViewPager
+import com.example.recyclomate.adapters.ViewPagerAdpater
+import com.example.recyclomate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.container.adapter = ViewPagerAdpater(supportFragmentManager)
+
+        // setting home as default
+        binding.container.currentItem = 1
+        binding.bottomNavigationView.menu.findItem(R.id.nav_home).isChecked = true
+
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_map -> {
+                    binding.container.currentItem = 0
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.nav_home -> {
+                    binding.container.currentItem = 1
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.nav_profile -> {
+                    binding.container.currentItem = 2
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                else -> {
+                    false
+                }
+            }
+        }
+
+        binding.container.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+
+            override fun onPageSelected(position: Int) {
+                binding.bottomNavigationView.menu.getItem(position).isChecked = true
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {}
+        })
     }
 }
---
+++ app/src/main/java/com/example/recyclomate/adapters/ViewPagerAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.recyclomate.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.recyclomate.UI.HomeFragment
+import com.example.recyclomate.UI.MapFragment
+import com.example.recyclomate.UI.ProfileFragment
+
+class ViewPagerAdpater(fragmentActivity: FragmentManager): FragmentPagerAdapter(fragmentActivity) {
+    override fun getCount(): Int {
+        return 3
+    }
+
+    override fun getItem(position: Int): Fragment {
+        when(position) {
+            0 -> {
+                return MapFragment()
+            }
+            1 -> {
+                return HomeFragment()
+            }
+            2 -> {
+                return ProfileFragment()
+            }
+            else -> {
+                return HomeFragment()
+            }
+        }
+    }
+
+
+}
