628b6af943fde70c79d72a702e8ffd5d98afde80
Ayush Kumar <ayushkr242003@gmail.com>
2024-10-18 00:12:34 +0530

    Image View Activity

--- app/src/main/java/com/example/recyclomate/GuideActivity.kt
+++ app/src/main/java/com/example/recyclomate/GuideActivity.kt
@@ -5,7 +5,6 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.recyclomate.R
-//import com.example.recyclomate.WebViewActivity
 import com.example.recyclomate.databinding.ActivityGuideBinding
 
 class GuideActivity : AppCompatActivity() {
@@ -20,23 +19,24 @@ class GuideActivity : AppCompatActivity() {
         binding = ActivityGuideBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        // Set text for waste management overview
         binding.wastemangmentoverview.text = getString(R.string.waste_management_overview)
 
-
+        // Set click listener for YouTube thumbnail
         binding.youtubeThumbnail5.setOnClickListener {
-
             val videoIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=dQw4w9WgXcQ"))
             startActivity(videoIntent)
         }
 
         // Set click listener for "Know More" text using binding
-//        binding.knowMore3.setOnClickListener {
-//            // Create an Intent to start WebViewActivity
-//            val intent = Intent(this, WebViewActivity::class.java)
-//            startActivity(intent)
-//        }
+        binding.knowMore3.setOnClickListener {
+            // Create an Intent to start WebViewActivity
+            val intent = Intent(this, WebViewActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
+
 
 
