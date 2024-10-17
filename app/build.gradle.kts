de1ea47fb8d42bb53f5b32ceab5bd1cc4ffb1438
Ayush Kumar <ayushkr242003@gmail.com>
2024-10-17 18:06:46 +0530

    Guide & Identification

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,6 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.firebase.bom)
     implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
 }
